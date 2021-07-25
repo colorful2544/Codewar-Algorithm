33ee2eff175d0a96c2e2bc02618496378e802142
Bike Chanokchon <84967704+colorful2544@users.noreply.github.com>
2021-07-25 16:24:02 +0700

    upload

---
+++ Valid Parentheses.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Valid_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidParentheses("()"));
+        }
+        public static bool ValidParentheses(string input)
+        {
+            int score = 0;
+            foreach(char i in input)
+            {
+                if(i == '(') score++;
+                if(i == ')') score--;
+                if(score < 0) return false;
+            }
+            return score == 0;
+        }
+    }
+}
