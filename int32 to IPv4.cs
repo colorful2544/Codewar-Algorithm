f83ac96e3a2c296f2bb1564f8b63819ad5fa7a45
Bike Chanokchon <84967704+colorful2544@users.noreply.github.com>
2021-07-29 19:14:19 +0700

    upload

---
+++ int32 to IPv4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace int32_to_IPv4
+{
+    //https://docs.microsoft.com/en-us/dotnet/api/system.net.ipaddress.parse?view=net-5.0
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UInt32ToIP(2154959208));
+        }
+        public static string UInt32ToIP(uint ip)
+        {
+            IPAddress address = IPAddress.Parse(ip.ToString());
+            return address.ToString();
+        }
+    }
+}
